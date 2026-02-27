fba79165fd9353c3d01e25339253b121e95079a8
jyutong65-ghb <jyutong65@gmail.com>
2026-02-27 12:26:25 +0300

    Practical 3-4

--- firstsite/sportsmen/admin.py
+++ firstsite/sportsmen/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sportsman
 
-# Register your models here.
+@admin.register(Sportsman)
+class SportsmanAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'time_create', 'is_published')
+    list_filter = ('is_published', 'time_create')
+    search_fields = ('title', 'content')
+
--- firstsite/sportsmen/urls.py
+++ firstsite/sportsmen/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('sports/', views.sports, name='sports'),
-    path('about/', views.about, name='about'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('sports/<int:sp_id>/', views.sports, name='sports_by_id'),
+    re_path(r'^sports/(?P<year>[0-9]{4})/$', views.sports_by_year, name='sports_by_year'),
 ]
